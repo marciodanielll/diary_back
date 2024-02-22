0dc2282764fc56f675bc7efd6b1bb6a85cb91ee1
M.D <marcio.daniel@msn.com>
2024-02-22 15:08:15 -0300

    feat(global): remove not used controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InfraModule } from './infra/modules';
 
 @Module({
   imports: [InfraModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
