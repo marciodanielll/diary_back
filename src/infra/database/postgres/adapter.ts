a314201409f274f9337f25374aa0698314e99a9d
M.D <marcio.daniel@msn.com>
2024-02-27 14:22:39 -0300

    chore(global): update packages

--- src/infra/database/postgres/adapter.ts
+++
@@ -1,9 +0,0 @@
-import { OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-
-export abstract class IDatabaseService
-  implements OnModuleInit, OnModuleDestroy
-{
-  abstract onModuleInit(): Promise<void>;
-  abstract onModuleDestroy(): Promise<void>;
-  abstract query<T>(text: string, params?: unknown[]): Promise<T>;
-}
--- src/infra/modules.ts
+++ src/infra/modules.ts
@@ -2,15 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { SecretsModule } from './secrets/module';
 import { LoggerModule } from './logger/module';
-import { DatabaseModule } from './database/postgres';
 import { PostgresDatabaseModule } from './database/postgres-type-orm/module';
 
 @Module({
-  imports: [
-    SecretsModule,
-    LoggerModule,
-    DatabaseModule,
-    PostgresDatabaseModule,
-  ],
+  imports: [SecretsModule, LoggerModule, PostgresDatabaseModule],
 })
 export class InfraModule {}
