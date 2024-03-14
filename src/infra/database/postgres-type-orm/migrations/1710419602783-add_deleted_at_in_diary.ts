682f5e465e16d745d4bc641b055268a60ff84021
M.D <marcio.daniel@msn.com>
2024-03-14 10:18:38 -0300

    feat(diary): create migration for add new fild

---
+++ src/infra/database/postgres-type-orm/migrations/1710419602783-add_deleted_at_in_diary.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddDeletedAtInDiaries1709082105104 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE diaries
+      ADD COLUMN deleted_at TIMESTAMP WITH TIME ZONE DEFAULT NULL;
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE diaries
+      DROP COLUMN deleted_at;
+    `);
+  }
+}
